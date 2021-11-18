934dd3195c4b36b40ab13bf81034448730a3fef5
ritaramada <anaritaramada@gmail.com>
2021-11-18 16:33:27 +0000

    Rules done [Missing text]

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ closeConf.onclick = function(){
     confPopUp.style.display = "none";
 }
 
+var rulesPopUp = document.getElementById("rulesPopup");
+var openRules = document.getElementById("rules");
+var closeRules = document.getElementById("closeRules");
+
+openRules.onclick = function(){
+    rulesPopUp.style.display = "block";
+}
+
+closeRules.onclick = function(){ 
+    rulesPopUp.style.display = "none";
+}
