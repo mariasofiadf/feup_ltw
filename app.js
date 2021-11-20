80e7a0f1e5756a7d73b126bf105e7676b67c614e
Maria Sofia Figueiredo <up201904675@fe.up.pt>
2021-11-20 12:09:16 +0000

    Board Resize working

--- app.js
+++ app.js
@@ -2,21 +2,35 @@
 
 window.onload = function() {
 
-    const board = new Board("board");
 
 }
 
-function createBoard(){
+function updateBoard(){
 
+    const oldBoard  = document.getElementById('board');
+    oldBoard.innerHTML = '';
+
+    const holes  = document.getElementById('holes').value;
+    const newBoard = new Board("board", holes);
 }
 
 class Board{
-    constructor(id){
+    constructor(id, holes){
         const board  = document.getElementById('board');
         for(let r = 0; r<2; r++){
             let row = document.createElement('tr');
-            for(let c = 0; c < 8; c++){
+            if(r==0){
+                let td = document.createElement('td');
+                td.rowSpan = 2;
+                row.appendChild(td)
+            }
+            for(let c = 0; c < holes; c++){
                 row.appendChild(document.createElement('td'))
+            }
+            if(r==0){
+                let td = document.createElement('td');
+                td.rowSpan = 2;
+                row.appendChild(td)
             }
             board.appendChild(row);
         }
