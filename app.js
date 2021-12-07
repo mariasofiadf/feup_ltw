c5674d4962ce62fe3c65230d06e88e3ec7c2c54b
Maria Sofia Figueiredo <up201904675@fe.up.pt>
2021-12-07 08:34:39 +0000

    executePlay

--- app.js
+++ app.js
@@ -49,35 +49,32 @@ function updateBoard(){
     const holes  = document.getElementById('holes').value;
     const seeds = document.getElementById('n_seeds').value;
     const newBoard = new Board("board", holes, seeds);
+    //newBoard.executePlay(0, 0);
 }
 
 class Board{
     constructor(id, holes, n_seeds){
         const board  = document.getElementById('board'); 
-        
-        this.buildStore1(board);
+        this.cellCount = holes;
+        this.cells = [];
+        this.stores = [];
+        this.cellsSeeds = [];
+        this.storesSeeds = [];
+        this.buildStore(board,0);
 
         this.buildCells(board, holes, n_seeds);
 
-        this.buildStore2(board);
+        this.buildStore(board,1);
 
     }
 
-    buildStore1(board){
-        this.store1 = document.createElement('div'); //Store for PLayer1
-        this.store1.className = "store";
-        board.appendChild(this.store1);
-        this.store_value1 = 0;
-        this.store1.innerHTML = this.store_value1;
+    buildStore(board, n){
+        this.stores[1-n] = document.createElement('div'); //Store for PLayer1
+        this.stores[1-n].className = "store";
+        this.stores[1-n].innerHTML = 0;
+        board.appendChild(this.stores[1-n]);
     }
 
-    buildStore2(board){
-        this.store2 = document.createElement('div'); //Store for PLayer1
-        this.store2.className = "store";
-        board.appendChild(this.store2);
-        this.store_value2 = 0;
-        this.store2.innerHTML = this.store_value2;
-    }
 
     buildCells(board, holes, n_seeds){
         const rows = document.createElement('div');//Container for the rows
@@ -87,13 +84,13 @@ class Board{
         for(let r = 0; r<2; r++){ //Create both rows
             let row = document.createElement('div');
             row.className = "row";
-            
+            this.cells.push([])
             for(let c = 0; c < holes; c++){
-                let cell =document.createElement('div');
-                cell.className = "cell";
-                cell.innerHTML = n_seeds;
+                this.cells[r][c] =document.createElement('div');
+                this.cells[r][c].className = "cell";
+                this.cells[r][c].innerHTML = n_seeds;
 
-                row.appendChild(cell);
+                row.appendChild(this.cells[r][c]);
 
             }
 
@@ -101,4 +98,17 @@ class Board{
         }
     }
 
+    executePlay(r, c){
+        let seeds = parseInt(this.cells[r][c].innerHTML);
+        this.cells[r][c].innerHTML = 0
+        c += 1;
+        if(r == 0){
+            while(c < this.cellCount && seeds > 0){
+                
+                this.cells[r][c].innerHTML += 1;
+                c++; seeds--;
+            }
+        }
+
+    }
 }
